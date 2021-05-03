41ffbb3b32ecf889d34e590ba672f89e40205428
kento-96 <kento.soccer.1107@gmail.com>
2021-05-03 12:45:55 +0000

    [create]relationships_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,23 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites,dependent: :destroy
+
   has_many :follower,class_name:"Relationship",foreign_key:"follower_id",dependent: :destroy #フォロー取得
-  has_many :following_user,through: :follower,source: :followed #自分がフォローしている人
-  
+  has_many :followings,through: :follower,source: :followed #自分がフォローしている人
+
   has_many :followed,class_name:"Relationship",foreign_key:"followed_id",dependent: :destroy #フォロワー取得
-  has_many :follower_user,through: :followed, source: :follower #自分をフォローしている人
+  has_many :followers,through: :followed, source: :follower #自分をフォローしている人
   attachment :profile_image
 
-
+ def follow(user_id)
+   relationships.create(followed_id: user_id)
+ end
+ def unfollow(user_id)
+   relationships.find_by(followed_id: user_id).destroy
+ end
+ def following?(user)
+   followings.include?(user)
+ end
   validates :name, uniqueness:true , presence:true, length: {maximum:20, minimum:2}
   validates :introduction,  length:{maximum:50}
 
